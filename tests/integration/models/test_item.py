3b45d670792a41ecddad40ac10e19320ad7dd0fe
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-12 13:55:01 +0100

    tests and configuration for sonar
    Configuring tests files for sonar and new integration test
    
    S5,L61

---
+++ tests/integration/models/test_item.py
@@ -0,0 +1,16 @@
+from models.item import ItemModel
+from tests.base_test import BaseTest
+
+
+class TestItem(BaseTest):
+    def test_crud(self):
+        with self.app_context():
+            item = ItemModel('test', 19.99)
+            self.assertIsNone(ItemModel.find_by_name('test'))
+            item.save_to_db()
+
+            self.assertIsNotNone(ItemModel.find_by_name('test'))
+
+            item.delete_from_db()
+
+            self.assertIsNone(ItemModel.find_by_name('test'))
